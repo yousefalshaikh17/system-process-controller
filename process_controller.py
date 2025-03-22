e488032335bfac5339efdded170c7c219b5a7eba
yousefalshaikh17 <164582835+yousefalshaikh17@users.noreply.github.com>
2025-03-22 01:02:02 +0000

    initial commit

---
+++ process_controller.py
@@ -0,0 +1,179 @@
+from typing import Optional
+
+import time
+import psutil
+import threading
+
+class ProcessController():
+    """
+    A class to manage and monitor system processes.
+    """
+
+    def __init__(self, process: psutil.Process):
+        """
+        Initializes the ProcessController with a given psutil Process object.
+
+        Parameters:
+        - process (psutil.Process): The psutil Process object to manage.
+        """
+        # Only PID and create time are stored for memory-efficiency.
+        self.pid = process.pid
+        self.create_time = process.create_time()
+        
+    @staticmethod
+    def find_processes(filters=None) -> list['ProcessController']:
+        """
+        Finds and returns a list of ProcessController instances matching the provided filters.
+        
+        Parameters:
+        - filters (dict?): A dictionary containing filter criteria as key-value pairs.
+          The available filter keys are:
+            - 'pid' (int): The process ID (PID) to filter by.
+            - 'name' (str): The name of the executable for the process (e.g., 'python.exe').
+            - 'cwd' (str): The current working directory of the process (e.g., 'D:/server').
+            - 'username' (str): The username under which the process is running (e.g., 'admin').
+            - 'create_time' (float): The creation time of the process (seconds since epoch).
+        
+        Returns:
+        - List of Process objects matching the criteria.
+
+        """
+        if not isinstance(process, psutil.Process):
+            raise TypeError("The process must be an instance of psutil.Process")
+
+        if filters is None:
+            filters = {}
+
+        processes = []
+        
+        # Iterate through all processes
+        for process in psutil.process_iter(['pid', 'name', 'cwd', 'username', 'create_time']):
+            match = True
+            
+            # Check each key-value pair in the filters
+            for key, value in filters.items():
+                if key not in process.info or process.info[key] != value:
+                    match = False
+                    break
+
+            if match:
+                processes.append(ProcessController(process))
+        
+        return processes
+    
+    def get_runtime(self) -> float:
+        """
+        Returns the time (in seconds) since the process was created.
+        """
+        return time.time() - self.create_time
+    
+    def get_process(self) -> Optional[psutil.Process]:
+        """
+        Returns the psutil Process object or None if unavailable.
+        """
+        try:
+            process = psutil.Process(self.pid)
+            # Create time is used to verify that another process hasn't reused the PID
+            if process.create_time() == self.create_time:
+                return process
+        except psutil.NoSuchProcess:
+            pass
+        except psutil.AccessDenied:
+            print(f"Access denied to process with PID: {self.pid}")
+        return None
+
+    def is_running(self) -> bool:
+        """
+        Returns whether the process is running.
+        """
+        process = self.get_process()
+        return process is not None and process.is_running()
+    
+    def get_cpu_usage(self, interval=0.1) -> Optional[float]:
+        """
+        Returns process CPU usage percentage (averaged over `interval`).
+        """
+        if not isinstance(interval, (float, int)):
+            raise TypeError("The interval must be a number (float or int)")
+
+        process = self.get_process()
+        return process.cpu_percent(interval=interval) if process else None
+
+    def get_memory_usage_mb(self) -> Optional[float]:
+        """
+        Returns the process memory usage in MB.
+        """
+        process = self.get_process()
+        return round(process.memory_info().rss / (1024 * 1024), 2) if process else None
+    
+    def close(self)-> bool:
+        """
+        Attempts to terminate the process safely.
+        """
+        process = self.get_process()
+        if process is not None:
+            try:
+               process.terminate()
+               return True
+            except psutil.NoSuchProcess:
+                return True
+            except (psutil.AccessDenied, psutil.TimeoutExpired, psutil.ZombieProcess, psutil.Error) as e:
+                print(f"An error occurred: {e}")
+        return False
+    
+    def terminate(self) -> bool:
+        """
+        Wrapper method around close to terminate the process safely.
+        """
+        return self.close()
+    
+    def terminate_after(self, delay, daemon=False) -> None:
+        """
+        Schedules the process for termination after a specified delay.
+
+        Parameters:
+        - delay (float): The time (in seconds) to wait before terminating the process.
+        - daemon (bool): Whether the termination should be executed in a separate thread (default: False).
+        """
+        if not isinstance(delay, (float, int)):
+            raise TypeError("The delay must be a number (float or int)")
+        
+        if not isinstance(daemon, bool):
+            raise TypeError("daemon value must be a boolean.")
+
+        def delayed_termination():
+            time.sleep(delay)
+            self.terminate()
+        
+        thread = threading.Thread(target=delayed_termination, daemon=daemon)
+        thread.start()
+
+    def restart(self) -> bool:
+        """
+        Restarts the process if it is running, terminating and then starting a new instance with the same command line and working directory.
+        """
+        process = self.get_process()
+        if process:
+            try:
+                # Store cmdline before termination
+                cmdline = process.cmdline()
+                cwd = process.cwd()
+
+                # If the process stopped before the restart, cancel restart attempt.
+                if not process.is_running():
+                    return False
+                
+                self.terminate()
+                
+                # Wait for thread to die
+                while self.is_running():
+                    time.sleep(0.1)
+
+                # Start thread
+                new_process = psutil.Popen(cmdline, cwd=cwd)
+                self.pid = new_process.pid
+                self.create_time = new_process.create_time
+                return True
+            except Exception as e:
+                print(f"Error restarting process {self.pid}: {e}")
+        return False
