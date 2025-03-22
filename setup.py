4e5e5eb90e58ea5188f400d6ea9efee5219feeab
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-03-22 16:13:05 +0000

    Adjusted package name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="process-controller",
+    name="processcontroller",
     version="0.4",
     packages=find_packages(),
     install_requires=[
